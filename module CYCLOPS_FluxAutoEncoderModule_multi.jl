b74dc41d306f92520473f47225494dac77bc6881
Nicolas Barra <nicolasbarra@vagelos-ve502-0283.apn.wlan.upenn.edu>
2019-06-05 16:33:23 -0400

    Create the model sketch

--- module CYCLOPS_FluxAutoEncoderModule_multi.jl
+++ module CYCLOPS_FluxAutoEncoderModule_multi.jl
@@ -1,8 +1,22 @@
-#module CYCLOPS_FluxAutoEncoderModule_multi
+module CYCLOPS_FluxAutoEncoderModule_multi
 
-W = randn(3, 5)
-b = zeros(3)
-x = rand(5)
-gradient(myloss, W, b, x)
+using Flux
+using CSV
 
-#end
+# circular activation function
+function circ(z::Float64,zstar::Float64)
+  z/(sqrt(z^2+zstar^2))
+end
+
+seed_homologues1 = CSV.read("Human_UbiquityCyclers.csv");
+
+#encoder = Dense(outs1, 2, x -> x)
+bottleneck = Dense(2, 2, circ)
+#decoder = Dense(2, outs1, x -> x)
+
+#model = Chain(encoder, bottleneck, decoder)
+
+loss(x) = mse(model(x), x)
+
+
+end
