3ab2a610f963af98a981b0d25ec93a47f5be45c6
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-06-11 17:21:25 -0400

    more use to address dataframe to array issues

--- scratch_code.jl
+++ scratch_code.jl
@@ -1,21 +1,37 @@
 using CSV
 
 fullnonseed_data = CSV.read("Annotated_Unlogged_BA11Data.csv")
-alldata_times = fullnonseed_data[3, 4:end]
+
+function makefloat!(x, df)
+    for col in x:size(df)[2]
+        if typeof(df[:, col]) != Array{Float64,1}
+            df[:, col] = map(x -> tryparse(Float64, x), df[:, col])
+        end
+    end
+end
+
+makefloat!(4, fullnonseed_data)
+
+fullnonseed_data_array = convert(Matrix, fullnonseed_data)
+
+println(fullnonseed_data_array)
+
+
+#alldata_times = fullnonseed_data[3, 4:end]
 
 #println(mean(alldata_data,2))
 
-alldata_data = fullnonseed_data[4:end, 4:end]
+#alldata_data = fullnonseed_data[4:end, 4:end]
 
 
 
 
 
-for col in 1:size(alldata_data)[2]
+#= for col in 1:size(alldata_data)[2]
     if typeof(alldata_data[:, col]) != Array{Float64,1}
         println("foooyy")
     end
-end
+end =#
 
 #show(alldata_data, allcols=true)
 
