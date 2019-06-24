0f73bc9f08eadfd9d10062c688321756180a414f
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-06-24 15:30:48 -0400

    saving model testing

--- scrratch_code3.jl
+++ scrratch_code3.jl
@@ -1,4 +1,5 @@
 using Flux, Juno
+using BSON: @save
 
 encoderA = Dense(10, 2)
 decoderB = Dense(2, 10)
@@ -10,7 +11,6 @@ testA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 testB = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 test = cat(testA, testB; dims=2)
 test1 = mapslices(x -> [x], test, dims=1)[:]
-
 
 loss(x) = Flux.mse(m1(x), x)
 
@@ -28,3 +28,4 @@ end
 estimated_phaselist = extractphase(test, m1)
 estimated_phaselist = mod.(estimated_phaselist .+ 2*pi, 2*pi)
 =#
+@save "m1.bson" m1
