6076ad5bc3c090f3295c8a86e92dd61904429407
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-06-26 11:27:46 -0400

    layer concatentation testing

--- scrratch_code3.jl
+++ scrratch_code3.jl
@@ -2,19 +2,21 @@ using Flux, Juno
 using BSON: @save
 
 encoderA = Dense(10, 2)
-decoderB = Dense(2, 10)
+circ(x) = x./sqrt(sum(x .* x))
+lin = Dense(2, 2)
+decoderB = Dense(4, 10)
 
 
-m1 = Chain(encoderA, decoderB)
+m1 = Chain(encoderA, x -> cat(circ(x), lin(x); dims = 1), decoderB)
 
 testA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 testB = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-test = cat(testA, testB; dims=2)
+test = cat(testA, testB; dims = 2)
 test1 = mapslices(x -> [x], test, dims=1)[:]
 
 loss(x) = Flux.mse(m1(x), x)
 
-Flux.@epochs 400 Flux.train!(loss, Flux.params(m1), zip(test1), ADAM(), cb = () -> println(string("Loss: ", loss(test))))
+Flux.@epochs 1 Flux.train!(loss, Flux.params(m1), zip(test1), ADAM(), cb = () -> println(string("Loss: ", loss(test))))
 #=
 function extractphase(data_matrix, model)
     points = size(data_matrix, 2)
@@ -28,4 +30,4 @@ end
 estimated_phaselist = extractphase(test, m1)
 estimated_phaselist = mod.(estimated_phaselist .+ 2*pi, 2*pi)
 =#
-@save "m1.bson" m1
+#@save "m1.bson" m1
