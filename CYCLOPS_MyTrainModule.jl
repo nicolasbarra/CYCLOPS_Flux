695b4a1155c2071c203821342bd3ab098cba968f
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-06-26 16:05:01 -0400

    adjust breakbpoint

--- CYCLOPS_MyTrainModule.jl
+++ CYCLOPS_MyTrainModule.jl
@@ -1,10 +1,12 @@
 module CYCLOPS_MyTrainModule
 
+#= This module offers modificatied versions of Flux's train! function and @epochs macro to suit our needs better for our neural network, and the releveant supporting code. Specifically, we modify train! so that it stores the loss over the entire epoch (which is the same as the instance it is called) and returns the average loss over that epoch, as well as printing it to the console to keep track of training progress. We modify @epochs so that it is able to store the output of the modified train function in an array and then return that array. This is useful for plotting how the average loss changes across epochs of training. Additionally, it includes a feature that will halt the inputted number of epochs of training if there are more than 5 consecutive epochs during which the average loss over a particular epoch increases compared to the previous epoch. =#
+
 import Statistics: mean
-import Flux.Tracker: Params, gradient, update!
+import Flux.Tracker: Params, gradient, data, update!
 import Juno: @progress
 
-export mytrain!
+export mytrain!, @myepochs
 
 call(f, xs...) = f(xs...)
 runall(f) = f
@@ -35,6 +37,22 @@ function mytrain!(loss, ps, data, opt; cb = () -> ())
   println(string("Average loss this epoch: ", avg))
 
   avg
+end
+
+macro myepochs(n, ex)
+  return :(lossrecord = [];
+  @progress for i = 1:$(esc(n))
+      @info "Epoch $i"
+      avgloss = $(esc(ex))
+      if length(lossrecord) > 5 && avgloss > avgloss[length(lossrecord)] && avgloss[length(lossrecord)] > avgloss[length(lossrecord) - 1] && avgloss[length(lossrecord) - 1] > avgloss[length(lossrecord) - 2] && avgloss[length(lossrecord) - 2] > avgloss[length(lossrecord) - 3]
+        break
+      else
+        append!(lossrecord, avgloss)
+      end
+    end;
+    lossrecord = map(x -> data(x), lossrecord);
+
+    lossrecord)
 end
 
 end  # module CYCLOPS_MyTrainModule
