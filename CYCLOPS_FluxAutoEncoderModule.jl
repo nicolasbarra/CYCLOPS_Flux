e2ea212d472eca48a35c996400ef77753610b4bb
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-06-28 15:56:00 -0400

    allowed for distince circular nodes

--- CYCLOPS_FluxAutoEncoderModule.jl
+++ CYCLOPS_FluxAutoEncoderModule.jl
@@ -12,23 +12,24 @@ function makeautoencoder(in_out_dim::Integer, n_circs::Integer, n_lins::Integer)
     elseif n_circs < 0 || n_lins < 0
         throw(ArgumentError("The number of circular nodes and linear layers in the bottleneck cannot be less than zero."))
     end
-    encoder = Dense(in_out_dim, 2)
+    encodercirc = Dense(in_out_dim, 2)
+    encoderlin = Dense(in_out_dim, 1)
     function circ(x)
       length(x) == 2 || throw(ArgumentError(string("Invalid length of input that should be 2 but is ", length(x))))
       x./sqrt(sum(x .* x))
     end
-    lin = Dense(2, 2)
-    function bottleneck(n_circs1, n_lins1)
+    lin = Dense(1, 1)
+    function encodetobottleneck(n_circs1, n_lins1)
         if n_circs1 == 0
-            x -> reduce(vcat, Iterators.repeated(lin(x), n_lins1))
+            x -> reduce(vcat, Iterators.repeated(encoderlin(lin(x)), n_lins1))
         elseif n_lins1 == 0
-            x -> reduce(vcat, Iterators.repeated(circ(x), n_circs1))
+            x -> reduce(vcat, Iterators.repeated(encodercirc(circ(x)), n_circs1))
         else
-            x -> vcat(reduce(vcat, Iterators.repeated(circ(x), n_circs1)), reduce(vcat, Iterators.repeated(lin(x), n_lins1)))
+            x -> vcat(reduce(vcat, Iterators.repeated(encodercirc(circ(x)), n_circs1)), reduce(vcat, Iterators.repeated(encoderlin(lin(x)), n_lins1)))
         end
     end
-    decoder = Dense(n_circs*2 + n_lins*2, in_out_dim)
-    model = Chain(encoder, bottleneck(n_circs, n_lins), decoder)
+    decoder = Dense(n_circs*2 + n_lins, in_out_dim)
+    model = Chain(encodetobottleneck(n_circs, n_lins), decoder)
 
     model
 end
