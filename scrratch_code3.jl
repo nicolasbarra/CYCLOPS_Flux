0658689baa667c6a7fec60d150b92ce795df48e7
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-06-28 12:55:11 -0400

    model making

--- scrratch_code3.jl
+++ scrratch_code3.jl
@@ -1,14 +1,21 @@
 using Flux
 using BSON: @save
 
-include("CYCLOPS_MyTrainModule.jl")
+import Random
+Random.seed!(12345)
+
+
+include("CYCLOPS_TrainingModule.jl")
 
 encoderA = Dense(10, 2)
 circ(x) = x./sqrt(sum(x .* x))
 lin = Dense(2, 2)
 decoderB = Dense(4, 10)
+function makebottle(n)
+    x -> reduce(vcat, Iterators.repeated(circ(x), n))
+end
 
-m1 = Chain(encoderA, x -> cat(circ(x), lin(x); dims = 1), decoderB)
+m1 = Chain(encoderA, makebottle(2), decoderB)
 
 testA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 testB = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -17,7 +24,7 @@ test1 = mapslices(x -> [x], test, dims=1)[:]
 
 loss(x) = Flux.mse(m1(x), x)
 
-a = CYCLOPS_MyTrainModule.mytrain!(loss, Flux.params(m1), zip(test1), ADAM(), cb = () -> println(string("Loss: ", loss(test))))
+a = CYCLOPS_TrainingModule.mytrain!(loss, Flux.params(m1), zip(test1), ADAM())
 #=
 function extractphase(data_matrix, model)
     points = size(data_mXatrix, 2)
