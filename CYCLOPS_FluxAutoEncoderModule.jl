2c23307b8e43ea07ddfdd69bd61af36f0768c501
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-07-01 16:55:12 -0400

    allow specification of lin layer dims

--- CYCLOPS_FluxAutoEncoderModule.jl
+++ CYCLOPS_FluxAutoEncoderModule.jl
@@ -3,17 +3,16 @@ module CYCLOPS_FluxAutoEncoderModule
 #= This module provides a function that creates a balanced autoencoder using Flux. Specifically, this function creates a circular node autoencoder where it is possible to specificy the number of circular nodes and linear nodes in the bottleneck layer in addition to the input (and thus also output since this is an balanced autoencoder) dimensions. It then returns a model that reflects these inputs that has been created using Flux's Chain function. The bottleneck layer is the concatenation of the specified number of circular and/or linear layers. =#
 
 import Flux: Dense, Chain
+import Flux.Tracker: data
 
 export makeautoencoder
 
-function makeautoencoder(in_out_dim::Integer, n_circs::Integer, n_lins::Integer)
+function makeautoencoder(in_out_dim::Integer, n_circs::Integer, n_lins::Integer, lin_dim::Integer)
     if n_circs == 0 && n_lins == 0
         throw(ArgumentError("The number of circular nodes and linear layers in the bottleneck cannot both be zero."))
     elseif n_circs < 0 || n_lins < 0
         throw(ArgumentError("The number of circular nodes and linear layers in the bottleneck cannot be less than zero."))
     end
-    encodercirc = Dense(in_out_dim, 2)
-    encoderlin = Dense(in_out_dim, 1)
     function circ(x)
       length(x) == 2 || throw(ArgumentError(string("Invalid length of input that should be 2 but is ", length(x))))
       x./sqrt(sum(x .* x))
@@ -21,11 +20,11 @@ function makeautoencoder(in_out_dim::Integer, n_circs::Integer, n_lins::Integer)
     lin = Dense(1, 1, x -> x)
     function encodetobottleneck(n_circs1, n_lins1)
         if n_circs1 == 0
-            x -> reduce(vcat, Iterators.repeated(lin(encoderlin(x)), n_lins1))
+            x -> reduce(vcat, Iterators.repeated(lin(Dense(in_out_dim, lin_dim)(x)), n_lins1))
         elseif n_lins1 == 0
-            x -> reduce(vcat, Iterators.repeated(circ(encodercirc(x)), n_circs1))
+            x -> reduce(vcat, Iterators.repeated(circ(Dense(in_out_dim, 2)(x)), n_circs1))
         else
-            x -> vcat(reduce(vcat, Iterators.repeated(circ(encodercirc(x)), n_circs1)), reduce(vcat, Iterators.repeated(lin(encoderlin(x)), n_lins1)))
+            x -> vcat(reduce(vcat, Iterators.repeated(circ(Dense(in_out_dim, 2)(x)), n_circs1)), reduce(vcat, Iterators.repeated(lin(Dense(in_out_dim, lin_dim)(x)), n_lins1)))
         end
     end
     decoder = Dense(n_circs*2 + n_lins, in_out_dim)
@@ -41,7 +40,7 @@ function extractphase(data_matrix, model, n_circs::Integer)
     base = 0
     for circ in 1:n_circs
         for n in 1:points
-            phases[circ, n] = Tracker.data(atan(model[1](data_matrix[:, n])[2 + base], model[1](data_matrix[:, n])[1 + base]))
+            phases[circ, n] = data(atan(model[1](data_matrix[:, n])[2 + base], model[1](data_matrix[:, n])[1 + base]))
         end
         base += 2
     end
