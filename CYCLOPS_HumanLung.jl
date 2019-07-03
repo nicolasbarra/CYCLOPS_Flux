fd8f028dbd910b41961a55ed1c9b6f0ccf9c11c9
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-07-03 13:01:53 -0400

    attempting one hot

--- CYCLOPS_HumanLung.jl
+++ CYCLOPS_HumanLung.jl
@@ -1,8 +1,17 @@
-using Distributed, CSV
+using Distributed, CSV, DataFrames
 
 @everywhere basedir = homedir()
 @everywhere cd(basedir * "/Downloads/Research")
 fullnonseed_data_laval = CSV.read("AnnotatedLAVALData_March3_2015.csv")
 fullnonseed_data_grng = CSV.read("AnnotatedGRNGData_March3_2015.csv")
-seed_homologues = CSV.read("LungCyclerHomologues.csv")
-homologue_symbol_list = seed_homologues[2:end,2]
+homologue_symbol_list = CSV.read("LungCyclerHomologues.csv")[2]
+
+#  add labels for one hot to each dataset
+fullnonseed_data_laval.label = 1
+fullnonseed_data_grng.label = 2
+
+#  eliminate duplicate rows
+deletecols!(fullnonseed_data_grng, [2, 3])
+
+#  join the the DataFrames
+fullnonseed_data = join(fullnonseed_data_laval, fullnonseed_data_grng, on = :Column1, makeunique = true)
