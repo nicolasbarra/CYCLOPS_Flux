20355aa96dda52b48288cda86de034fa79c8ea0a
Nicolas Barra <nb.nicolasbarra@gmail.com>
2019-07-15 09:25:28 -0400

    scratch code

--- scratch_code6.jl
+++ scratch_code6.jl
@@ -3,13 +3,24 @@ function circ(x)
   x./sqrt(sum(x .* x))
 end
 
-makecircs(in_out_dim1, n_circs1) = y -> begin
-circlayers = []
-for i in 1:n_circs1
-      @eval $(Symbol("encoderbottle_$i")) = Chain(Dense(in_out_dim1, 2), circ)
+function makecircs(in_out_dim1, n_circs1)
+  for i in 1:n_circs1
+    @eval $(Symbol("encoderbottle_$i")) = Chain(Dense(in_out_dim1, 2), circ)
+  end
+  modelmakerstring = "y -> vcat(u)"
+  u = "encoderbottle_1(y)"
+  if n_circs1 > 1
+    for i in 2:n_circs1
+      u = u * ", encoderbottle_$i(y)"
+    end
+  end
+  modelmakerstring = modelmakerstring[1:findfirst(isequal('u'), modelmakerstring)-1] * u * modelmakerstring[findfirst(isequal('u'), modelmakerstring)+1:end]
+
+  Symbol(modelmakerstring)
 end
-for i in 1:n_circs1
-  :(encoderbottle_$i)
+
+
+
 
 for i in 1:n_circs1
   append!(circlayers, (x -> circ(Dense(in_out_dim1, 2)(x))))
@@ -45,3 +56,17 @@ makelins(in_out_dim1, lin_dim1, n_lins1) = x -> vcat( collect(Iterators.repeated
 foo = x -> circ(a(x))
 endcode = vcat(foo, foo)
 endcode(g)
+
+using Flux
+function circ(x)
+  length(x) == 2 || throw(ArgumentError(string("Invalid length of input that should be 2 but is ", length(x))))
+  x./sqrt(sum(x .* x))
+end
+function foog(in_out_dim, n_circs)
+  for i in 1:n_circs
+          @eval $(Symbol("encoderbottle_$i")) = Chain(Dense($in_out_dim, 2), circ)
+  end
+end
+
+
+@eval $(Symbol("f = x -> x^2"))
